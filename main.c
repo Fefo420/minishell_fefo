0d0553dee57c91e677e36b5a232e9fc6b443bb5d
Julio Max Calderone Da Silva <julcalde@1-E-23.42heilbronn.de>
2025-03-13 11:48:41 +0100

    ignore

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fileonar <fileonar@student.42.fr>          +#+  +:+       +#+        */
+/*   By: julcalde <julcalde@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 18:13:02 by julcalde          #+#    #+#             */
-/*   Updated: 2025/03/12 12:31:12 by fileonar         ###   ########.fr       */
+/*   Updated: 2025/03/13 11:38:50 by julcalde         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,44 +14,42 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-
-char *check_for_commands(char	*input)
+char	*check_for_commands(char *input)
 {
-	char *echo_history;
+	char	*echo_history;
 
 	if (input)
 	{
 		if (ft_strncmp(input, "echo", 4) == 0)
 		{
-			//skip the echo command and the space
+			// skip the echo command and the space
 			input += 5;
-			//print the rest of the input
+			// print the rest of the input
 			ft_putstr_fd(input, 1);
 			ft_putstr_fd("\n", 1);
 			echo_history = ft_strjoin("echo ", input);
-			//add the input to the history
+			// add the input to the history
 			return (echo_history);
 		}
 		else if (ft_strncmp(input, "clear", 5) == 0)
 		{
-			//clear the screen
+			// clear the screen
 			rl_clear_history();
 			return (input);
 		}
-
 	}
 	return (input);
 }
 
 int	main(void)
 {
-	//get usename, print it and use it as prompt
-	char *user;
-	char *prompt;
-	char *input;
-	char *history;
+	// get usename, print it and use it as prompt
+	char	*user;
+	char	*prompt;
+	char	*input;
+	char	*history;
 
-	//The getenv function is used to retrieve the value of an environment variable.
+	// The getenv function is used to retrieve the value of an environment variable.
 	user = getenv("USER");
 	if (!user)
 		user = "user";
@@ -59,7 +57,7 @@ int	main(void)
 	{
 		prompt = ft_strjoin(user, "_minishell$ ");
 		input = readline(prompt);
-		//start building echo
+		// start building echo
 		history = check_for_commands(input);
 		add_history(history);
 	}
